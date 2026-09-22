87947808cb48f01db42f978c04cc023136bb26f0
ihshan1015 <mohamedihshan1015@gmail.com>
2026-09-22 22:33:01 +0530

    Connect frontend to deployed backend

--- project/frontend/src/services/api.js
+++ project/frontend/src/services/api.js
@@ -1,7 +1,5 @@
 import axios from 'axios';
-
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
-
+const API = axios.create({ baseURL: 'https://capstone-project-q1xl.onrender.com/api' });
 API.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem('token');
